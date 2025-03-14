0ef3d52eaed817afc7b0c1121b2f5a6dae06f030
oussama <oussamatght6@gmail.com>
2025-03-14 23:16:49 +0100

    Update ouss.js

--- ouss.js
+++ ouss.js
@@ -1,6 +1,6 @@
 let x = document.querySelector(".ui");
-let y = document.querySelector(".login");
-let z = document.querySelector(".signe");
+let y = document.querySelector("#login");
+let z = document.querySelector("#signe");
 let r = document.querySelector(".si");
 
 console.log(x, y, z, r);
